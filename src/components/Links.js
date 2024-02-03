d07fa2b4812f1d2705dce336f278d07df5442b32
Rebekah Ndanu <rebekah.ndanu@student.moringaschool.com>
2024-02-03 13:35:59 +0300

    Completed assignment

--- src/components/Links.js
+++ src/components/Links.js
@@ -4,9 +4,7 @@ function Links(props) {
   return (
     <div>
       <h3>Links</h3>
-      {/* Display the URL of the GitHub link */}
       <a href={props.github}>{props.github}</a>
-      {/* Display the URL of the LinkedIn link */}
       <a href={props.linkedin}>{props.linkedin}</a>
     </div>
   );
